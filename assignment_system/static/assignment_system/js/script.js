ceb4c5c20f9b57dbf5a203a6f35e37381cbb6a9d
Nazar910 <pyvovarnazar@gmail.com>
2018-03-12 01:45:50 +0200

    fixes

--- assignment_system/static/assignment_system/js/script.js
+++ assignment_system/static/assignment_system/js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function () {
-    console.log('Here');
-});
+function printDiv(divId){
+    console.log('Divid', divId);
+    const printContents = document.getElementById(divId).innerHTML;
+    const originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
